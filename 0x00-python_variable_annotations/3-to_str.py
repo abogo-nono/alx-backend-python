21df9ab00af24ebbf7daf3dac49a0175ad691ee8
Kalash <kalash@KaRed.kc.org>
2024-10-11 23:37:15 +0100

    all task done

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Basic annotations - to string  """
+
+from typing import List, Tuple
+
+
+def to_str(n: float) -> str:
+    """ Returns the string representation of the float. """
+    return str(n)
